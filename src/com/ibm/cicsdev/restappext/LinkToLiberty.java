469e4afb6dd2067351781e9cad08aeeea87b5f6e
tcsusr1 <tcsusr1@tcs001.dfw.ibm.com>
2024-01-05 01:30:08 -0600

    "commited8"

--- src/com/ibm/cicsdev/restappext/LinkToLiberty.java
+++ src/com/ibm/cicsdev/restappext/LinkToLiberty.java
@@ -59,7 +59,7 @@ public class LinkToLiberty
         supplier.setSupplierId(iSupplierId);
         
         // Create a new generated supplier name
-        String name = "Priya123 #" + iSupplierId;
+        String name = "Uma#" + iSupplierId;
         supplier.setSupplierName(name);
         
         // Store in a return container
